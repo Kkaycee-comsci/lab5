b6a8b21fd446632cffc349aa4fe56a2a032ce856
unknown <evangelistak596@gmail.com>
2026-02-28 12:00:59 +0800

    Final connection fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: 'http://localhost:3000'
+// This is the bridge between your website and your cloud server
+const API = axios.create({
+    // Make sure this matches the link Railway gave you!
+    baseURL: 'https://lab5-production.up.railway.app/api' 
 });
+
+export default API;
